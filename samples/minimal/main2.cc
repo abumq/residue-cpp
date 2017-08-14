85d820b1c77ad46e3153cdbc15561bdac2194d83
mkhan <mkhan@allhomes.com.au>
2017-08-14 16:26:24 +1000

    main2 sample

---
+++ samples/minimal/main2.cc
@@ -0,0 +1,24 @@
+#include <Residue.h>
+
+int main(int argc, char** argv) {
+    Residue::setApplicationArgs(argc, argv);
+
+    if (el::Helpers::commandLineArgs()->hasParamWithValue("--conf")) {
+        Residue::loadConfiguration(el::Helpers::commandLineArgs()->getParamValue("--conf"));
+    }
+
+    Residue::connect();
+
+    system("date");
+    int count = 1000000;
+    for (int i = 1; i <= count; ++i) {
+        LOG(INFO) << "Message #" << i << " from residue sample";
+    }
+    std::cout << "Already finished the logger - just flushing it to the server." << std::endl;
+
+    system("date");
+
+    Residue::disconnect();
+
+    return 0;
+}
