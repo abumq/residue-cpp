4cbc58061660ecefb3d86509f196f0decc19742c
mkhan <mkhan@allhomes.com.au>
2018-03-26 11:06:59 +1100

    u

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 VERSION=`curl https://api.github.com/repos/muflihun/residue-cpp/releases | grep tag_name | head -n1 | grep -o "[0-9]\.[0-9]\.[0-9]"`
+
 DAR=`uname -a | grep 'Darwin' | wc -c | grep -o '[0-9]'`
 TYPE=darwin
 if [ "$DAR" = "0" ];then
     TYPE=linux
+fi
+
+if [ "$LINUX_SPECIAL_EDITION" != "" ];then
+    TYPE=linux-se
 fi
 
 ## Headers
@@ -19,11 +24,13 @@ cp libresidue-$VERSION-headers/* /usr/local/include/residue/
 wget https://github.com/muflihun/residue-cpp/releases/download/v$VERSION/libresidue-$VERSION-x86_64-$TYPE.tar.gz
 tar -xf libresidue-$VERSION-x86_64-$TYPE.tar.gz
 cp libresidue-$VERSION-x86_64-$TYPE/libresidue* /usr/local/lib/libresidue$DEST_SUFFIX
+rm /usr/local/lib/libresidue.$EXTENSION
 ln -s /usr/local/lib/libresidue$DEST_SUFFIX /usr/local/lib/libresidue.$EXTENSION
 
 ## Static lib
 wget https://github.com/muflihun/residue-cpp/releases/download/v$VERSION/libresidue-$VERSION-static-x86_64-$TYPE.tar.gz
 tar -xf libresidue-$VERSION-static-x86_64-$TYPE.tar.gz
 cp libresidue-$VERSION-static-x86_64-$TYPE/libresidue-static.$VERSION.a /usr/local/lib/
+rm /usr/local/lib/libresidue-static.a
 ln -s /usr/local/lib/libresidue-static.$VERSION.a /usr/local/lib/libresidue-static.a
 
